e321707e160df4d37ed966a8ce35bc5c11e36f58
Anton Gilgur <agilgur5@gmail.com>
2019-02-25 01:20:18 -0500

    (feat): persist favorites (and mangas)
    - just persisting favorites doesn't work as it only saves the refs, so
      have to persist all the manga O_O
      - should definitely figure out how to fix this in a later commit
    
    - persistence logic copied from https://gist.github.com/benjick/c48dd2db575e79c7b0b1043de4556ebc
      - aside from one noted change to get it to work properly

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'mobx-react'
 
 import AppModel from './models/models.js'
+import { persist } from './utils/persist'
 import MainView from './components/mainView.js'
 
 const appStore = AppModel.create()
+
+persist('@mangaStoreKey', appStore, {
+  storage: AsyncStorage,
+  jsonify: true // set to true if using AsyncStorage
+}, {
+  mangas: true,
+  favorites: true
+})
 
 const App = () => (
   <Provider appStore={appStore}>
