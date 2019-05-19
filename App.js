286725f417d321f25d16ee3858b0e7e6b7886e77
Anton Gilgur <agilgur5@gmail.com>
2019-05-19 05:42:06 -0400

    (refactor/deps): extract out mst-persist into its own lib
    - made a bunch of changes, but works in mostly the same fashion
      - mostly additions and superificial changes to the underlying code
        - e.g. whitelists + blacklists (and as arrays, like redux-persist),
          persist returns promise instead of calling `afterHydration`, etc
    
    - wanted lib per https://github.com/pinqy520/mobx-persist/issues/37 ,
      but only gists and examples around
      - it's a tiny wrapper library for now, but still faster than writing
        by yourself
    
    - installing via GH since empty NPM package is owned by someone else rn
      - https://github.com/pinqy520/mobx-persist/issues/37#issuecomment-361479253

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import persist from 'mst-persist'
 import { AsyncStorage } from 'react-native'
 import { Provider } from 'mobx-react'
 
 import AppModel from './models/models.js'
-import { persist } from './utils/persist'
 import MainView from './components/mainView.js'
 
 const appStore = AppModel.create()
 
 persist('@mangaStoreKey', appStore, {
   storage: AsyncStorage,
-  jsonify: true // set to true if using AsyncStorage
-}, {
-  mangas: true,
-  favorites: true
+  jsonify: true, // set to true if using AsyncStorage
+  whitelist: [
+    'mangas',
+    'favorites'
+  ]
 })
 
 const App = () => (
